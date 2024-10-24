2b44815b15df7e575f3eeee2510ef0d3eab7e725
Antonio Avila <v-antonio.avila@aceable.com>
2024-10-23 19:03:14 -0600

    Converter implementation

--- src/routes/Converter.tsx
+++ src/routes/Converter.tsx
@@ -3,39 +3,84 @@ import "./Converter.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export function Converter(/* props */) {
+const API_URL = 'http://localhost:3000/currencies';
 
+export function Converter(/* props */) {
+  const [base, setBase] = useState('');
+  const [target, setTarget] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [currencies, setCurrencies] = useState([]); 
+  const [rate, setRate] = useState(0);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+
+        setCurrencies(Object.entries(data) as any);
+    });      
+  }, []);
+
+  useEffect(() => {
+    if (base !== '' && target !=='') {
+      fetch(`${API_URL}/base/${base}/target/${target}`)
+        .then(res => res.json())
+        .then(data => {
+
+          setRate(data.rate);
+      });      
+    }
+  }, [base, target]);
+
+  const onChangeSelect = (ev: any) => {
+    const {name, value} = ev.target;
+    name == 'targetcurrency' ?  setTarget(value) : setBase(value)
+  }
 
   return (
     <div className="root-container">
       <div className="search-currency">
         <input
           type="text"
-          value={100}
+          value={amount}
           placeholder="Amount"
+          onChange={({ target }) => setAmount(Number(target.value))}
         />
-        <span>{123}</span>
+        <span>
+          Conversion result: {
+            new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(rate*amount)
+          }
+        </span>
       </div>
       <div className="currencies-container">
         <div className="selectors">
           <select
             name="basecurrency"
             id="basecurrency"
+            onChange={onChangeSelect}
           >
-            <option key={1} value={123}>
-              {123}
-            </option>
+            {currencies.map(val => {
+              const [key, label] = val;
+
+                 return <option key={key} value={key}>
+                                {label}
+                              </option>
+            })}
+            
           </select>
         </div>
         <div className="selectors">
           <select
             name="targetcurrency"
             id="targetcurrency"
+            onChange={onChangeSelect}
           >
-            <option key={321} value={321}>
-              {321}
-            </option>
-            );
+            {currencies.map(val => {
+              const [key, label] = val;
+                 return <option key={key} value={key}>
+                                {label}
+                              </option>
+            })}
           </select>
         </div>
       </div>
